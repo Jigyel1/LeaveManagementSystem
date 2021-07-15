e0efb7066f4175a74dd4cee0141160597cbc7652
unknown <gyeljiyeshey.dorjee@gmail.com>
2021-07-14 18:20:32 -0700

    Seed Changes!

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.create!  :email => 'admin@admin.com', :password => 'admin@123', :password_confirmation => 'admin@123', :role => 0
+user = User.create! :fname => 'Tester', :lname => '1', :dob => '17/12/1997', :phone => '17617232', :designation => 'Teacher', :address => 'Babesa, Thimphu', :email => 'tester1@gmail.com', :password => 'test@123', :password_confirmation => 'test@123', :role => 2
